fb25bb468cc88437f26c3f310e7bee36eea99483
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-07 01:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function prependKitten(name) {
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function removeLastKitten() {
-  return kittens.slice(-1)
+  return kittens.slice(0, kittens.length -1)
 }
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
