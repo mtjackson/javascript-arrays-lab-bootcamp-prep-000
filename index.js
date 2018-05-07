480704db8d1406c883c3b59ce4b83a76b9e19523
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-07 01:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
 
-var destructivelyAppendKitten = [name]
-
-destructivelyAppendKitten.push(kitten)
+function destructivelyAppendKitten(name) {
+  return destructivelyAppendKitten.push(name)
+}
